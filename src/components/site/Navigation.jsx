e2ae3f0451f6049cce7a64f6d8d35697b1e74cf6
John <john_graves@brown.edu>
2021-02-02 23:39:11 -0500

    add logo

--- src/components/site/Navigation.jsx
+++ src/components/site/Navigation.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
 import { Navbar,Nav,NavDropdown,Form,FormControl,Button} from 'react-bootstrap';
-import './main.css';
 
 
 function Navigation() {
   return (
     <div className="navigation">
-      <Navbar className={"navbar navbar-dark"} expand="lg">
-        <Navbar.Brand href="/">viper games</Navbar.Brand>
+      <Navbar className={"navbar navbar-dark"} style={{backgroundColor: "green"}} expand="lg">
+        <Navbar.Brand href="/">
+          <img width="100%" height="100%" src="https://raw.githubusercontent.com/jgraves123/ESCovid-2.0/master/images/ship.jpg?raw=true"/>
+          VIPER games</Navbar.Brand>
         <Navbar.Toggle/>
         <Navbar.Collapse className="justify-content-end">
           <Nav>
